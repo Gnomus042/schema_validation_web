a877f5787a360ebd3c71cb0c9ac30b6c91fa751d
Anastasiia Byvsheva <anastasiiabyvsheva@MacBook-Pro.local>
2020-07-17 15:16:39 +0300

    Tests added, interface improved

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 var path = require('path');
+var fs = require('fs');
 var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/views/sdtt.html'))
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/sdtt.html'))
 })
 
 app.get('/test', function (req, res) {
-  res.sendFile(path.join(__dirname + '/views/test.html'));
+    res.sendFile(path.join(__dirname + '/views/test.html'));
+});
+
+app.get('/sdtt/tests', function (req, res) {
+    let tests = [];
+    fs.readdir('public/shex/tests', (err, files) => {
+        files.forEach((file, idx) => {
+            let test = {id: idx, link: `shex/tests/${file}`}
+            tests.push(test);
+        });
+        res.json(tests);
+    })
+
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port 3000!');
 });
--- public/javascripts/sdtt.js
+++ public/javascripts/sdtt.js
@@ -1,4 +1,46 @@
 const shexURL = location + "shex/full.shex";
+let context;
+let tests;
+
+$.get('shex/context.json', data => {
+    context = data;
+});
+
+$(document).ready(() => {
+    $.get('sdtt/tests', data => {
+        tests = data;
+        let testsBlock = $('.tests');
+        tests.forEach(test => testsBlock.append(
+            `<div class="test btn btn-light" onclick="setTest('${test.link}')">Test ${test.id}</div>`
+        ));
+    });
+})
+
+function setTest(testLink) {
+    $.get(testLink, data => {
+        $("#text-data-input").val(JSON.stringify(data, undefined, 2))
+    }).then(err => console.log(err));
+}
+
+
+$(document).delegate('#text-data-input', 'keydown', function(e) {
+    var keyCode = e.keyCode || e.which;
+
+    if (keyCode == 9) {
+        e.preventDefault();
+        var start = this.selectionStart;
+        var end = this.selectionEnd;
+
+        // set textarea value to: text before caret + tab + text after caret
+        $(this).val($(this).val().substring(0, start)
+            + "\t"
+            + $(this).val().substring(end));
+
+        // put caret at right position again
+        this.selectionStart =
+            this.selectionEnd = start + 1;
+    }
+});
 
 $("#input-type").change(() => {
     $('.inputs').toggleClass('d-sm-none');
@@ -32,7 +74,7 @@ function getInput() {
     if ($("#input-type").val() === "text") {
         let resolver;
         let promise = new Promise((res, rej) => resolver = res);
-        resolver($("text-data-input").val());
+        resolver($("#text-data-input").val());
         return promise;
     }
     let file = $("#file-data-input").get(0).files[0];
@@ -47,16 +89,34 @@ $("#validate-btn").on('click', () => {
                 alert("Please add the @id field");
                 return;
             }
+            data['@context'] = context;
             let dataId = data["@id"];
             let dataURL = stringToUrl(JSON.stringify(data))
             let errors = validation.validateShEx(dataURL, shexURL, dataId, "http://schema.org/shex#GoogleRecipe");
             let warnings = validation.validateShEx(dataURL, shexURL, dataId, "http://schema.org/shex#GoogleRecipeStrict");
             Promise.all([errors, warnings]).then(res => {
+                let errors = simplifyResult(res[0]);
+                let warnings = simplifyResult(res[1]);
                 printResults(res[0], res[1]);
             })
-        })
-        .catch(err => console.log("getInput() error: " + err));
+        });
 });
+
+function removeUrl(url) {
+    let slashIdx = url.lastIndexOf("/");
+    return url.substr(slashIdx+1);
+}
+
+function simplifyResult(result) {
+    if (result.errors) {
+        let errors = [];
+        result.errors.forEach(err => {
+            errors = errors.concat(simplifyResult(err));
+        });
+        return errors;
+    }
+    return [result]
+}
 
 function printResults(errors, warnings) {
     $(".results-wrapper").removeClass('d-sm-none');
@@ -66,3 +126,7 @@ function printResults(errors, warnings) {
     $("#errors").text(JSON.stringify(errors, undefined, 2));
     $("#warnings").text(JSON.stringify(warnings, undefined, 2));
 }
+
+$("#shex-btn").on('click', () => {
+   window.location.replace(window.location  + "/shex/full.shex");
+});
